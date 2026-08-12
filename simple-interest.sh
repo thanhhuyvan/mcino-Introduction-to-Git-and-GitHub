79c05a495e9bf4564f41f99801a1b13360271368
thanhhuyvan <thanhhuyvan5@gmail.com>
2026-08-13 00:23:32 +0700

    Fix typo and add contributor username

--- simple-interest.sh
+++ simple-interest.sh
@@ -3,8 +3,8 @@
 # Do not use this in production. Sample purpose only.
 
 # Author: Upkar Lidder (IBM)
-# Addtional Authors:
-# <your Github username>
+# Additional Authors:
+# thanhhuyvan
 
 # Input:
 # p, principal amount
